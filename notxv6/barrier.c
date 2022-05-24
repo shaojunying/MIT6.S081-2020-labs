4d29955b28ea0e848bbb4395b97f330498e0893b
shaojunying <shaojunying1@gmail.com>
2022-05-24 21:58:15 +0800

    实现了Barrier实验

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -25,12 +25,18 @@ barrier_init(void)
 static void 
 barrier()
 {
-  // YOUR CODE HERE
-  //
-  // Block until all threads have called barrier() and
-  // then increment bstate.round.
-  //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  bstate.nthread ++;
+  if (nthread == bstate.nthread) {
+    // 达到了最大数量，直接唤醒其他进程并退出
+    pthread_cond_broadcast(&bstate.barrier_cond);
+    bstate.round ++;
+    bstate.nthread = 0;
+  }else {
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  }
+  // 释放锁，并返回即可
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
