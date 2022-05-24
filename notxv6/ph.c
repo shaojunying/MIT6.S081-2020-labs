74d8fbb548c1cfcacce7a4bc25c93fae1130aa04
shaojunying <shaojunying1@gmail.com>
2022-05-24 22:53:16 +0800

    完成了ph实验

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -14,6 +14,7 @@ struct entry {
   struct entry *next;
 };
 struct entry *table[NBUCKET];
+pthread_mutex_t lock[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
@@ -39,6 +40,7 @@ static
 void put(int key, int value)
 {
   int i = key % NBUCKET;
+  pthread_mutex_lock(&lock[i]);
 
   // is the key already present?
   struct entry *e = 0;
@@ -53,19 +55,21 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(&lock[i]); 
 }
 
 static struct entry*
 get(int key)
 {
   int i = key % NBUCKET;
+  pthread_mutex_lock(&lock[i]); 
 
 
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
-
+  pthread_mutex_unlock(&lock[i]); 
   return e;
 }
 
@@ -109,6 +113,9 @@ main(int argc, char *argv[])
   }
   nthread = atoi(argv[1]);
   tha = malloc(sizeof(pthread_t) * nthread);
+  for (int i = 0; i < NBUCKET; i ++) {
+    pthread_mutex_init(&lock[i], NULL);
+  }
   srandom(0);
   assert(NKEYS % nthread == 0);
   for (int i = 0; i < NKEYS; i++) {
